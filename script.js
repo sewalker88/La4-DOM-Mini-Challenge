b3d0f7ffef968ed2dd28875902fa6f4aa389ba37
Shawna Walker <sewalker88@yahoo.com>
2020-08-04 17:21:44 -0400

    second commit for lab 4 forgot erase cross out

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 "use strict";
 //-------------------------#1-------------------------------------
-// let currentTotal = 0;
-// let snacks = document.querySelectorAll(".snacks");
-// let total = document.querySelector(".total");
-// console.dir(snacks);
+let currentTotal = 0;
+let snacks = document.querySelectorAll(".snacks");
+let total = document.querySelector(".total");
+console.dir(snacks);
 
-// //snacks.forEach((snack) => {
-// // snack.addEventListner("click", () => {
-// // let amount = parseInt(snack.getAttribute("data-amount"));
-// //currentTotal += amount;
-// //total.innerText = `TOTAL:$${currentTotal}`;
-// //});
-// //});
+snacks.forEach((snack) => {
+  snack.addEventListner("click", () => {
+    let amount = parseInt(snack.getAttribute("data-amount"));
+    currentTotal += amount;
+    total.innerText = `TOTAL:$${currentTotal}`;
+  });
+});
 
 // //-------------------------#2-----------------------------------------
 
-// let makeMoneyButton = document.querySelector(".Make Money");
-// makeMoneyButton.innerText = makeMoney;
-// let coinsButton = document.querySelector(".coins");
-// coinsButton.innerText = coins;
+let makeMoneyButton = document.querySelector(".Make Money");
+makeMoneyButton.innerText = makeMoney;
+let coinsButton = document.querySelector(".coins");
+coinsButton.innerText = coins;
 //-------------------#3----------------------
 let bulb = document.querySelector("bulb");
 let onButton = document.querySelector("on");
